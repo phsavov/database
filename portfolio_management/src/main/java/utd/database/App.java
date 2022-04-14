0330c6a8a32cbb0665b66137b56e17229c1adcc8
justin-schwinn-0 <justinsch1029@gmail.com>
2022-04-14 18:36:54 -0500

    many changes

--- portfolio_management/src/main/java/utd/database/App.java
+++ portfolio_management/src/main/java/utd/database/App.java
@@ -31,8 +31,39 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) 
+    {
+        //launch();
+        test();    
+    }
+
+    public static void test()
+    {
+        try
+        {
+            DatabaseController db = new DatabaseController();
+
+
+            /*String[] testALL = db.RawInputQuery("SELECT * FROM UserAccount;");
+            for(String s : testALL) {
+                System.out.println(s);
+            }
+
+            /*boolean testLogin = db.LoginUser("user", "Pass");
+            system.out.println("Logged in: "testLogin);
+
+            String[] testHoldings = db.ViewHoldings("5");
+            for(String s : testHoldings)
+            {
+                System.out.println(s);
+            }
+            */
+            db.CloseDBC();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getStackTrace());
+        }
     }
 
 }
--- portfolio_management/src/main/java/utd/database/DatabaseController.java
+++ portfolio_management/src/main/java/utd/database/DatabaseController.java
@@ -1,8 +1,6 @@
 package utd.database;
 import java.sql.*;
 
-import javax.naming.spi.DirStateFactory.Result;
-
 public class DatabaseController {
     
     private Connection connection;
@@ -12,13 +10,13 @@ public class DatabaseController {
     private static String SELL_STOCK_QUERY = "";
     private static String TRADE_STOCK_QUERY = "";
 
-    public DatabaseController() throws SQLexcpetion{
+    public DatabaseController() throws SQLException{
         System.out.println("Hello, world");
 
-        connection = DriverManager.getConnection("https//:127.0.0.1", "user", "password");
+        connection = DriverManager.getConnection("ads-postgres.cm0vx7gau7uz.us-east-1.rds.amazonaws.com", " phsavov", "PhiLeTo2001BL$");
     }
 
-    public void CloseDBC()
+    public void CloseDBC() throws SQLException
     {
         connection.close();
     }
@@ -26,13 +24,13 @@ public class DatabaseController {
     // takes a single string and send 
     // that unprocessed string as a query to the database
     // good for sql injection
-    public ResultSet RawInputQuery(String query)
+    public String[] RawInputQuery(String query) throws SQLException
     {
         Statement s = connection.createStatement();
-        return s.executeQuery(query);
+        return ResultsettoStringArray(s.executeQuery(query));
     }
 
-    public ResultSet GenericSelect(String[] colms, String from, String extras)
+    public ResultSet GenericSelect(String[] colms, String from, String extras) throws SQLException
     {
         Statement ps = connection.createStatement();
         StringBuilder sb = new StringBuilder();
@@ -43,7 +41,7 @@ public class DatabaseController {
         }
         sb.append(colms[colms.length-1]);
         sb.append("\nFROM " + from);
-        if(extra != null)
+        if(extras != null)
         {
             sb.append("\n"+ extras +";");
         }
@@ -53,18 +51,18 @@ public class DatabaseController {
         return ps.executeQuery(sb.toString());
     }
 
-    public boolean BuyStock(String accID,String stockID,int quantity)
+    public int BuyStock(String accID,String stockID,int quantity) throws SQLException
     {
         PreparedStatement ps = connection.prepareStatement(BUY_STOCK_QUERY);
         ps.setString(1, accID);
         ps.setString(2, stockID);
-        ps.setString(3, quantity);
+        ps.setInt(3, quantity);
 
         return ps.executeUpdate();
     }
 
     // hopefully safe version of user login
-    public boolean LoginUser(String user, String Pass)
+    public boolean LoginUser(String user, String Pass) throws SQLException
     {
         PreparedStatement ps = connection.prepareStatement(LOGIN_QUERY);
         ps.setString(1, user);
@@ -76,30 +74,16 @@ public class DatabaseController {
     //Returns a string array of every hold an account has
     // in the format
     // holdings[x] = "StockId,Quantity,Price,CurrentValue";
-    public String[] ViewHoldings(String accId)
+    public String[] ViewHoldings(String accId) throws SQLException
     {
         PreparedStatement ps = connection.prepareStatement(VIEW_HOLDINGS_QUERY);
 
         ps.setString(1, accId);
 
-        ResultSet r = ps.executeQuery();
-        StringBuilder sb = new StringBuilder();
-
-        while(r.next())
-        {
-            sb.append(r.getString(1)+",");
-            sb.append(r.getString(2)+",");
-            sb.append(r.getString(3)+",");
-            sb.append(r.getString(4)+",");
-            sb.append(r.getString(5)+"\n");
-        }
-
-        String[] rows = sb.toString().split("\n");
-
-        return rows;
+        return ResultsettoStringArray(ps.executeQuery());
     }
 
-    public boolean SellStocks(String accId,String stockid)
+    public int SellStocks(String accId,String stockid) throws SQLException
     {
         PreparedStatement ps = connection.prepareStatement(SELL_STOCK_QUERY);
 
@@ -107,5 +91,33 @@ public class DatabaseController {
         ps.setString(2, stockid);
 
         return ps.executeUpdate();
+    }
+
+    public int TradeStocks(String toAccID, String fromAccID, String stockID, int count) throws SQLException
+    {
+        PreparedStatement ps = connection.prepareStatement(TRADE_STOCK_QUERY);
+
+        ps.setString(1, toAccID);
+        ps.setString(2, fromAccID);
+        ps.setString(3, stockID);
+        ps.setInt(4, count);
+
+        return ps.executeUpdate();
+    }
+
+    private String[] ResultsettoStringArray(ResultSet r) throws SQLException
+    {
+        StringBuilder sb = new StringBuilder();
+
+        while(r.next())
+        {
+            for (int i = 1; i < r.getMetaData().getColumnCount(); i++) 
+            {
+                sb.append(r.getString(i)+ ",");
+            }
+            sb.append(r.getString(r.getMetaData().getColumnCount())+"\n");
+        }
+
+        return sb.toString().split("\n");
     }
 }
